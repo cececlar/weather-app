dbe6ea59fc0116b0e0b2e11276c56c377f143a64
Cece Claridge <cece.claridge@gmail.com>
2019-07-12 18:04:20 -0400

    made changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,61 @@
-import React from 'react';
-import Titles from './components/Titles';
-import Form from './components/Form';
-import Weather from './components/Weather';
+import React from "react";
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "afd42fbb8ebbeef811bca424254b1bec";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: undefined
-    }
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
   }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_key = "";
-    const url = `https://bypasscors.herokuapp.com/api/?url=http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`;
-    const api_call = await fetch(url);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     if (city && country) {
-    console.log(data);
-    this.setState(
-      {
+      this.setState({
         temperature: data.main.temp,
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
         description: data.weather[0].description,
         error: ""
-      })
-      } else {
-        this.setState(
-        {
+      });
+    } else {
+      this.setState({
         temperature: undefined,
         city: undefined,
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: "Please enter the value"
-      })
-      }
+        error: "Please enter the values."
+      });
+    }
   }
   render() {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
+        <Form getWeather={this.getWeather} />
         <Weather
           temperature={this.state.temperature}
+          humidity={this.state.humidity}
           city={this.state.city}
           country={this.state.country}
-          humidity={this.state.humidity}
           description={this.state.description}
           error={this.state.error}
-          />
+        />
       </div>
-    )
+    );
   }
-}
+};
 
 export default App;
